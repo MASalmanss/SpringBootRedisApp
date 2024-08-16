a3b8cf6b6544c244e49c45004994dc30a6043e34
mehmetakifsalman <ahmetakifsalman@gmail.com>
2024-08-16 14:38:31 +0300

    application.yml created , RedisConfig added

---
+++ RedisSpring/src/main/java/org/example/redisspring/config/RedisConfig.java
@@ -0,0 +1,33 @@
+package org.example.redisspring.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
+
+@EnableCaching
+@Configuration
+public class RedisConfig {
+    @Value("${cache.config.entryTtl:60}")
+    private int entryTtl;
+
+    @Value("${cache.config.countryNames.entryTtl:30}")
+    private int countryNamesEntryTtl;
+
+    @Bean
+    RedisCacheConfiguration redisCacheConfiguration(){
+        return RedisCacheConfiguration
+                .defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(entryTtl))
+                .disableCachingNullValues()
+                .serializeValuesWith(RedisSerializationContext
+                        .SerializationPair
+                        .fromSerializer(new GenericJackson2JsonRedisSerializer()));
+    }
+
+}
